b709d77a373b40fdd280c8f9704345cf18c7de20
Alexander Kastil <alexander.kastil@integrations.at>
2025-04-23 07:42:33 +0200

    openapi

--- demos/02-semantic-kernel/03-plugins/02-open-api/readme.md
+++ demos/02-semantic-kernel/03-plugins/02-open-api/readme.md
@@ -1,3 +1,9 @@
 # OpenAPI Plugins
 
 [OpenAPI Plugins](https://learn.microsoft.com/en-us/semantic-kernel/concepts/plugins/adding-openapi-plugins)
+
+[Swagger Editor](https://editor.swagger.io/)
+
+## Demo
+
+- Currency Converter is an Azure Function Node that containers a `swagger.json` file
---
+++ demos/02-semantic-kernel/03-plugins/02-open-api/sk-openapi/Program.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.SemanticKernel;
+
+var builder = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+IConfiguration configuration = builder.Build();
+
+var model = configuration["Model"];
+var endpoint = configuration["Endpoint"];
+var key = configuration["ApiKey"];
+
+var kernelBuilder = Kernel.CreateBuilder();
+
+kernelBuilder.Services.AddAzureOpenAIChatCompletion(
+   model,
+   endpoint,
+   key
+);
+
+var kernel = kernelBuilder.Build();
+
+while (true)
+{
+    Console.ForegroundColor = ConsoleColor.White;
+    Console.WriteLine("\nEnter a prompt (or 'exit' to quit):");
+    var prompt = Console.ReadLine();
+
+    if (string.IsNullOrEmpty(prompt) ||
+        string.Equals(prompt, "exit", StringComparison.OrdinalIgnoreCase))
+    {
+        break;
+    }
+
+    var result = await kernel.InvokePromptAsync(prompt);
+
+    Console.ForegroundColor = ConsoleColor.Yellow;
+    Console.WriteLine(result);
+}
